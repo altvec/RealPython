7758402300a6dd5bb10c3e1515b21eb2344ca7d7
altvec <sergey@bluehat.me>
2014-01-03 18:29:44 +0600

    Added task description

--- 4.2_temperature.py
+++ 4.2_temperature.py
@@ -1,3 +1,24 @@
+'''
+Task description:
+=================
+
+Write a script "temperature.py" that includes two functions.
+One function takes a Celsius temperature as its input, converts that
+number into the equivalent Fahrenheit temperature and returns that value.
+The second function takes a Fahrenheit temperature and returns the
+equivalent Celsius temperature.
+Test your functions by passing input values to them and printing the
+output results.
+
+For testing your functions, example output should look like:
+  72 degrees F = 22.2222222222 degrees C
+  37 degrees C = 98.6 degrees F
+In case you didn't want to spend a minute searching the web or doing
+algebra (the horror!), the relevant conversion formulas are:
+F = C * 9/5 + 32
+C = (F – 32) * 5/9
+'''
+
 from __future__ import division
 
 def cels_to_fahr(cels):
@@ -5,13 +26,10 @@ def cels_to_fahr(cels):
     result = cels * 9 / 5 + 32
     return result
 
-
-
 def fahr_to_cels(fahr):
     ''' Converts temp in fahrenheit to celsius '''
     result = (fahr - 32) * 5 / 9
     return result
-
 
 print "{} degrees F = {} degrees C".format(72, fahr_to_cels(72))
 print "{} degress C = {} degrees F".format(37, cels_to_fahr(37))
