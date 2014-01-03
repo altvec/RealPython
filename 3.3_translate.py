991d1cbc02d1fea20567076cea498781f45dab1c
altvec <sergey@bluehat.me>
2014-01-03 17:41:48 +0600

    Assignment 3.3

---
+++ 3.3_translate.py
@@ -0,0 +1,10 @@
+user_input = raw_input("Enter some text: ")
+translated = user_input.replace("a", "4")
+translated = translated.replace("b", "8")
+translated = translated.replace("e", "3")
+translated = translated.replace("l", "1")
+translated = translated.replace("o", "0")
+translated = translated.replace("s", "5")
+translated = translated.replace("t", "7")
+print translated
+
