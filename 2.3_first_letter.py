4499170cd1fcfa0743f9c950abe2324b9d95fcbc
altvec <sergey@bluehat.me>
2014-01-03 17:59:04 +0600

    Added task description

--- 2.3_first_letter.py
+++ 2.3_first_letter.py
@@ -1,2 +1,18 @@
+'''
+Task description:
+=================
+
+Write a script named “first_letter.py” that first prompts the user for
+input by using the string:
+    Tell me your password:
+The script should then determine the first letter of the user's input,
+convert that letter to upper-case, and display it back.
+As an example, if the user input was “no” then the program should
+respond like this:
+    The first letter you entered was: N
+For now, it's okay if your program crashes when the user enters nothing
+as input (just hitting ENTER instead).
+'''
+
 user_prompt = raw_input("Tell me your password: ")
 print "The first letter you entered was:", user_prompt[0].upper()
