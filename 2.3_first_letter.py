3f56dbaba33466f8963d63b08b44fa72640cad43
altvec <sergey@bluehat.me>
2014-01-03 18:26:27 +0600

    Fixed quotes

--- 2.3_first_letter.py
+++ 2.3_first_letter.py
@@ -2,12 +2,12 @@
 Task description:
 =================
 
-Write a script named “first_letter.py” that first prompts the user for
+Write a script named "first_letter.py" that first prompts the user for
 input by using the string:
     Tell me your password:
 The script should then determine the first letter of the user's input,
 convert that letter to upper-case, and display it back.
-As an example, if the user input was “no” then the program should
+As an example, if the user input was "no" then the program should
 respond like this:
     The first letter you entered was: N
 For now, it's okay if your program crashes when the user enters nothing
