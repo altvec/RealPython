74c01ac4c47f0e0df0835c2cc257dfe6306c99a7
altvec <sergey@bluehat.me>
2014-01-03 18:44:18 +0600

    Assignment 6.1

---
+++ 6.1_poetry.py
@@ -0,0 +1,131 @@
+'''
+Task description:
+=================
+
+Write a script "poetry.py" that will generate a poem based on randomly
+chosen words and a pre-determined structure. When you are done, you will
+be able to generate poetic masterpieces such as the following in mere
+milliseconds:
+
+A furry horse
+
+A furry horse curdles within the fragrant mango
+extravagantly, the horse slurps
+the mango meows beneath a balding extrovert
+
+All of the poems will have this same general structure, inspired by
+Clifford Pickover:
+
+{A/An} {adjective1} {noun1}
+
+{A/An} {adjective1} {noun1} {verb1} {preposition1} the {adjective2} {noun2}
+{adverb1}, the {noun1} {verb2}
+the {noun2} {verb3} {preposition2} a {adjective3} {noun3}
+
+Your script should include a function makePoem() that returns a multi-line
+string representing a complete poem. The main section of the code should
+simply print makePoem() to display a single poem. In order to get there,
+use the following steps as a guide:
+
+- First, you'll need a vocabulary from which to create the poem. Create
+several lists, each containing words pertaining to one part of speech
+(more or less); i.e., create separate lists for nouns, verbs, adjectives,
+adverbs, and prepositions. You will need to include at least three
+different nouns, three verbs, three adjectives, two prepositions and one
+adverb. You can use the sample word lists below, but feel free to add
+your own:
+
+Nouns: "fossil", "horse", "aardvark", "judge", "chef", "mango",
+"extrovert", "gorilla"
+
+Verbs: "kicks", "jingles", "bounces", "slurps", "meows", "explodes",
+"curdles"
+
+Adjectives: "furry", "balding", "incredulous", "fragrant", "exuberant",
+"glistening"
+
+Prepositions: "against", "after", "into", "beneath", "upon", "for", "in",
+"like", "over", "within"
+
+Adverbs: "curiously", "extravagantly", "tantalizingly", "furiously",
+"sensuously"
+
+- Choose random words from the appropriate list using the random.choice()
+function, storing each choice in a new string. Select three nouns,
+three verbs, three adjectives, one adverb, and two prepositions.
+Make sure that none of the words are repeated.
+(Hint: Use a while loop to repeat the selection process until you get a
+    new word.)
+- Plug the words you selected into the structure above to create a poem
+string by using the format() string method
+- Bonus: Make sure that the "A" in the title and the first line is
+adjusted to become an "An" automatically if the first adjective begins
+with a vowel.
+'''
+
+from random import choice
+
+noun = ["fossil", "horse", "aardvark", "judge", "chef", "mango",
+        "extrovert", "gorilla"]
+verb = ["kicks", "jingles", "bounces", "slurps", "meows",
+        "explodes", "curdles"]
+adjective = ["furry", "balding", "incredulous", "fragrant",
+             "exuberant", "glistening"]
+preposition = ["against", "after", "into", "beneath", "upon", "for",
+               "in", "like", "over", "within"]
+adverb = ["curiously", "extravagantly", "tantalizingly",
+          "furiously", "sensuously"]
+
+def makePoem():
+    ''' create a randomly generated poem, returned as a multi-line string'''
+    # Pull three nouns randomly
+    n1 = choice(noun)
+    n2 = choice(noun)
+    n3 = choice(noun)
+    # Make sure that all the nouns are different
+    while n1 == n2:
+        n2 = choice(noun)
+    while n1 == n3 or n2 == n3:
+        n3 = choice(noun)
+
+    # Pull three different verbs
+    v1 = choice(verb)
+    v2 = choice(verb)
+    v3 = choice(verb)
+    while v1 == v2:
+        v2 = choice(verb)
+    while v1 == v3 or v2 == v3:
+        v3 = choice(verb)
+
+    # Pull three different adjectives
+    adj1 = choice(adjective)
+    adj2 = choice(adjective)
+    adj3 = choice(adjective)
+    while adj1 == adj2:
+        adj2 = choice(adjective)
+    while n1 == n3 or n2 == n3:
+        adj3 = choice(adjective)
+
+    # Pull two different prepositions
+    prep1 = choice(preposition)
+    prep2 = choice(preposition)
+    while prep1 == prep2:
+        prep2 = choice(preposition)
+
+    # Pull one adverb
+    adv1 = choice(adverb)
+
+    if "aeiou".find(adj1[0]) != -1: # first letter is a vowel
+        article = "An"
+    else:
+        article = "A"
+
+    # add lines to poem
+    poem = "{} {} {}\n\n".format(article, adj1, n1)
+    poem = poem + "{} {} {} {} {} the {} {}\n".format(article, adj1, n1, v1, prep1, adj2, n2)
+    poem = poem + "{}, the {} {}\n".format(adv1, n1, v2)
+    poem = poem + "the {} {} {} a {} {}".format(n2, v3, prep2, adj3, n3)
+
+    return poem
+
+print makePoem()
