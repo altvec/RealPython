ea2affb7f3100f492c7f3ab8df7fdc6a5d6743b7
altvec <sergey@bluehat.me>
2014-01-05 00:26:44 +0600

    Task description for assignment 7.2

---
+++ 7.2_remove_files.py
@@ -0,0 +1,20 @@
+'''
+Task description:
+=================
+
+Write a script "remove_files.py" that will look in the chapter 7
+practices files folder named "little pics" as well all of its subfolders.
+The script should use os.remove() to delete any JPG file found in any of
+these folders if the file is less than 2 Kb (2,000 bytes) in size.
+
+You can supply the os.path.getsize() function with a full file path to
+return the file's size in bytes. Check the contents of the folders before
+running your script to make sure that you delete the correct files;
+you should only end up removing the files named "to be deleted.jpg" and
+"definitely has to go.jpg" - although you should only use the file
+extensions and file sizes to determine this.
+
+If you mess up and delete the wrong files, there is a folder named
+"backup" that contains an exact copy of the "little pics" folder and all
+its contents so that you can copy these contents back and try again.
+'''
