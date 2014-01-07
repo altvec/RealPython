70a7646cf9df9a272bc73cba18d14f84d20b472e
altvec <sergey@bluehat.me>
2014-01-07 23:46:19 +0600

    Assignment 8.2

--- 8.2_cover_the_emperor.py
+++ 8.2_cover_the_emperor.py
@@ -8,3 +8,24 @@ named "Emperor cover sheet.pdf" and outputs the full resulting PDF to the
 file "The Covered Emperor.pdf" in the chapter 8 practice files Output
 folder.
 '''
+
+import os
+from pyPdf import PdfFileReader, PdfFileWriter
+
+path = "/Users/srg/practice_files"
+inputFileName = os.path.join(path, "The Emperor.pdf")
+inputFile = PdfFileReader(file(inputFileName, "rb"))
+coverFileName = os.path.join(path, "Emperor cover sheet.pdf")
+coverFile = PdfFileReader(file(coverFileName, "rb"))
+outputPDF = PdfFileWriter()
+
+for pageNum in xrange(0, inputFile.getNumPages()):
+    page = inputFile.getPage(pageNum)
+    outputPDF.addPage(page)
+
+outputPDF.addPage(coverFile.getPage(0))
+
+outputFileName = os.path.join(path, "Output/The Covered Emperor.pdf")
+outputFile = file(outputFileName, "wb")
+outputPDF.write(outputFile)
+outputFile.close()
