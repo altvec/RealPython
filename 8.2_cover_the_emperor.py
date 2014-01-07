19982d4d9e6624a5e341ca466548ed39d8b72c62
altvec <sergey@bluehat.me>
2014-01-07 18:53:45 +0600

    Assignment 8.2 description

---
+++ 8.2_cover_the_emperor.py
@@ -0,0 +1,10 @@
+'''
+Task description:
+=================
+
+Write a script "cover_the_emperor.py" that appends the chapter 8 practice
+file named "The Emperor.pdf" to the end of the chapter 8 practice file
+named "Emperor cover sheet.pdf" and outputs the full resulting PDF to the
+file "The Covered Emperor.pdf" in the chapter 8 practice files Output
+folder.
+'''
