7b09e8ffd85698fac6046b40295f0d3db4548716
altvec <sergey@bluehat.me>
2014-01-07 23:48:33 +0600

    More description

--- 8.2_cover_the_emperor.py
+++ 8.2_cover_the_emperor.py
@@ -7,6 +7,9 @@ file named "The Emperor.pdf" to the end of the chapter 8 practice file
 named "Emperor cover sheet.pdf" and outputs the full resulting PDF to the
 file "The Covered Emperor.pdf" in the chapter 8 practice files Output
 folder.
+
+Obviously we need required PDF files to work with. They can be found in
+book materials.
 '''
 
 import os
