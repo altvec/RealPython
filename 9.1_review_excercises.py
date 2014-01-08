0325f6c002ff7ff52a263a718e585f3a42f40433
altvec <sergey@bluehat.me>
2014-01-09 00:38:57 +0600

    Base functionality

--- 9.1_review_excercises.py
+++ 9.1_review_excercises.py
@@ -13,3 +13,24 @@ Tasks description:
 - Display the names and IQs of everyone in the table who is classified
 as Human.
 '''
+
+import sqlite3
+
+defaultData = (
+    ("Jean-Baptiste Zorg", "Human", 122),
+    ("Korben Dallas", "Meat Popsicle", 100),
+    ("Ak'not", "Mangalore", -5)
+)
+
+connection = sqlite3.connect(':memory:')
+c = connection.cursor()
+c.execute("DROP TABLE IF EXISTS Roster")
+c.execute("CREATE TABLE Roster(Name TEXT, Species TEXT, IQ INT)")
+c.executemany("INSERT INTO Roster VALUES(?, ?, ?)", defaultData)
+
+# print table
+c.execute("SELECT * FROM Roster")
+for row in c.fetchall():
+    print row
+
+connection.close()
