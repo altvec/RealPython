655b92f9e0455640fe565d8e837850f2c2885050
altvec <sergey@bluehat.me>
2014-01-09 00:11:32 +0600

    Initial with task desc

---
+++ 9.1_review_excercises.py
@@ -0,0 +1,15 @@
+'''
+Tasks description:
+==================
+
+- Create a database table in RAM named 'Roster' that includes the fields
+'Name', 'Species' and 'IQ'
+- Populate you new table with the following values:
+    Jean-Baptiste Zorg, Human, 122
+    Korben Dallas, Meat Popsicle, 100
+    Ak'not, Mangalore, -5
+
+- Update the Species of Korben Dallas to be Human
+- Display the names and IQs of everyone in the table who is classified
+as Human.
+'''
