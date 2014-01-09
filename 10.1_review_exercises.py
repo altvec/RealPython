0856b94542a60bec563d930a64519a7e6b47dab4
altvec <sergey@bluehat.me>
2014-01-10 01:57:03 +0600

    Task description

---
+++ 10.1_review_exercises.py
@@ -0,0 +1,14 @@
+'''
+Task description:
+=================
+
+- Write script that grabs the full HTML from the page dionysus.html
+(http://www.realpython.com/practice/dionysus.html)
+- Using the string find() method to display the text following "Name:"
+and "Favorite Color:" (not including any leading spaces or trailing HTML
+tags might apper on the same line)
+- Repeat the previous exercise using regular expressions; the end of each
+pattern should be a "<" (i.e., the start of an HTML tag) or a newline
+character, and you should remove any extra spaces of newline characters
+from the resulting text
+'''
