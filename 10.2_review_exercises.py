aabeea84fd4e6f46ce26aa04fa1c2f0f578f71e9
altvec <sergey@bluehat.me>
2014-01-12 23:21:22 +0600

    More

--- 10.2_review_exercises.py
+++ 10.2_review_exercises.py
@@ -14,13 +14,14 @@ using Beautiful Soup's get_text() method
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
-addr = "http://www.realpython.com/practice/profiles.html"
+base_url = "http://www.realpython.com/practice/"
+addr = base_url + "profiles.html"
 html_page = urlopen(addr)
 html_text = html_page.read()
 parsed_text = BeautifulSoup(html_text)
 
 for link in parsed_text.find_all("a"):
-    link_addr = addr[:-13] + link["href"]
+    link_addr = base_url + link["href"]
     link_page = urlopen(link_addr)
     link_text = link_page.read()
     parsed_link = BeautifulSoup(link_text)
