8811059e5cfe47003671546d3d51fb169e88e3bb
altvec <sergey@bluehat.me>
2014-01-13 21:37:21 +0600

    Added task description

---
+++ 10.3_review_exercises.py
@@ -0,0 +1,15 @@
+'''
+Task description:
+=================
+
+– Use mechanize to provide the correct username "zeus" and password
+"ThunderDude" to the login page submission form located at:
+http://RealPython.com/practice/login.php
+– Using BeautifulSoup, display the title of the current page to determine
+that you have been redirected to profiles.html
+– Use mechanize to return to login.php by going "back" to the previous
+page
+– Provide an incorrect username and password to the login form, then
+search the HTML of the returned webpage for the text "Wrong username or
+password! to determine that the login process failed.
+'''
