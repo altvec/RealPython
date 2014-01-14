0e3e57f51e5cd9782ba0de36acaa8dd3023f692d
altvec <sergey@bluehat.me>
2014-01-15 00:25:41 +0600

    More

--- 10.3_review_exercises.py
+++ 10.3_review_exercises.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Task description:
 =================
@@ -13,3 +14,29 @@ page
 search the HTML of the returned webpage for the text "Wrong username or
 password! to determine that the login process failed.
 '''
+
+import mechanize
+from bs4 import BeautifulSoup
+
+browser = mechanize.Browser()
+htmlPage = browser.open("http://realpython.com/practice/login.php")
+browser.select_form("login")
+browser["user"] = "zeus"
+browser["pwd"] = "ThunderDude"
+response = browser.submit()
+data = response.get_data()
+parser = BeautifulSoup(data)
+print parser.title.string
+print response.geturl()
+
+browser.back()
+browser.select_form("login")
+browser["user"] = "none"
+browser["pwd"] = "lame_pass"
+response = browser.submit()
+data = response.get_data()
+
+if response.get_data().find("Wrong username or password!") != -1:
+    print "Login failed"
+else:
+    print "Login successful."
