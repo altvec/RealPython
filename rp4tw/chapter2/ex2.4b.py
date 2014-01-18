ce92bac58e9be9d5139b9825f1c42f632204ed25
altvec <sergey@bluehat.me>
2014-01-18 23:24:12 +0600

    some more from chapter 2

---
+++ rp4tw/chapter2/ex2.4b.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+with sqlite3.connect("employees.db") as connection:
+    c = connection.cursor()
+    c.execute("SELECT firstname, lastname FROM employees")
+    rows = c.fetchall()
+
+    for row in rows:
+        print row[0], row[1]
