a1bc8427461468612fa2f13c2836d10ab7bd84fc
altvec <sergey@bluehat.me>
2014-02-09 20:57:06 +0600

    Added authorization for blog

--- rp4tw/chapter5/blog/blog.py
+++ rp4tw/chapter5/blog/blog.py
@@ -2,26 +2,66 @@ from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
 import time
+from functools import wraps
 
 # Configuration
 DATABASE = 'fblog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'VSng16ohBH3MjdOe0ftdCmit83bzR9IgoqVh7oGz7BC9suJfdEyfOQ90wEaRgcrS'
 
 app = Flask(__name__)
 
 # Pulls in configuration by looking for UPPERCASE vars
 app.config.from_object(__name__)
 
+
 # Functions used for connection to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if (request.form['username'] != app.config['USERNAME'] or
+                request.form['password'] != app.config['PASSWORD']):
+            error = 'Invalid credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
+
+@app.route("/logout")
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash("You need to login first.")
+            return redirect(url_for('login'))
+    return wrap
+
 
 @app.route("/main")
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * FROM posts')
+    posts = [dict(date=row[0],
+                  title=row[1],
+                  post=row[2]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
