f394057aeeb146a010cf66f0b2762cdad4e4a362
altvec <sergey@bluehat.me>
2014-02-14 00:41:44 +0600

    Max length for username is 3, for password - 5. Registration works

--- rp4tw/chapter5/flasktaskr/app/forms.py
+++ rp4tw/chapter5/flasktaskr/app/forms.py
@@ -6,11 +6,11 @@ from wtforms.validators import Required, EqualTo, Length
 
 class RegisterForm(Form):
     name = TextField('Username',
-                     validators=[Required(), Length(min=6, max=25)])
+                     validators=[Required(), Length(min=3, max=25)])
     email = TextField('Email',
                       validators=[Required(), Length(min=6, max=60)])
     password = PasswordField('Password',
-                             validators=[Required(), Length(min=6, max=40)])
+                             validators=[Required(), Length(min=5, max=40)])
     confirm = PasswordField('Repeat Password',
                             [Required(),
                              EqualTo('password', message='Passwords must match')
